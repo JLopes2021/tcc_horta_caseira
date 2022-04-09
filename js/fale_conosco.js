e678581f8887d6497bb46bc1b3adca7644428475
jeffinho.je8@gmail.com <jeffinho.je8@gmail.com>
2022-04-09 16:34:31 -0300

    logica pre banco

--- js/fale_conosco.js
+++ js/fale_conosco.js
@@ -1,9 +1,28 @@
-function Enviar() {
-
+function envia() {
     var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    var email = document.getElementById("emailid");
+    var msg = document.getElementById("msgid");
+    if (nome.value != "" && msg.value != "" && email.value != "" ) {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso, a sua mensagem é: ' + msg.value + ' e o seu email é: ' + email.value );
     }
-
 }
+
+/*
+function salvar1Numero(numero, sit) {
+  const url = 'https://fireapp-d4454-default-rtdb.firebaseio.com/tabela.json'
+  const options = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'content-type': 'application/json;charset=utf-8'
+      },
+      body: `{
+        "numero": "${numero}",
+        "sit": "${sit}"
+       }`,
+  }
+
+
+   
+*/
