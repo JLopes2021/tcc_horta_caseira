f9f4f7c5ddf3ef6a321e72085d6c974d8242d8da
jeffinho.je8@gmail.com <jeffinho.je8@gmail.com>
2022-04-13 19:01:20 -0300

    arquivo de mudança de cores

---
+++ js/modos_cores.js
@@ -0,0 +1,12 @@
+function darkMode() {
+    var element = document.body;
+    var content = document.querySelector('mudaCor');
+    element.className = "dark-mode";
+ 
+
+  }
+  function lightMode(){
+    var element = document.body;
+    var content = document.getElementById("mudaCor");
+    element.className = "light-mode";
+  }
