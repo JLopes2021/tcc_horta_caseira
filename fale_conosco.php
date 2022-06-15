5348f6348dbf22a15d58a18d018e08de8e6d9ae8
Jefferson Lopes <81269229+JLopes2021@users.noreply.github.com>
2022-06-15 17:27:34 -0300

    update de meta charset

--- fale_conosco.php
+++ fale_conosco.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Fale Conosco</title>
         <link rel="shortcut icon" type="imagex/png" href="fotos/Logo Tcc.png">
