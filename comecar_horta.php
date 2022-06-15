14f6c60333eeac6b32b470abd2dbbdf12eee2f7e
Jefferson Lopes <81269229+JLopes2021@users.noreply.github.com>
2022-06-15 17:26:28 -0300

    update de meta charset

--- comecar_horta.php
+++ comecar_horta.php
@@ -4,7 +4,7 @@
 <html lang="pt-br">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Começar uma Horta</title>
     <link rel="shortcut icon" type="imagex/png" href="fotos/Logo Tcc.png">
