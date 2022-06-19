37627318f684cc006e0584a963fabc9063e98926
LucasMaciel17 <88008791+LucasMaciel17@users.noreply.github.com>
2022-06-19 18:59:08 -0300

    caracteres
    ver se ta certinho no seu

--- conexao.php
+++ conexao.php
@@ -4,6 +4,7 @@
  $senha="";
  $dbname="minha-horta";
  $conexao=mysqli_connect($servidor, $usuario, $senha, $dbname);
+ mysql_set_charset('utf8',$link);
  if(!$conexao){
    die("houve um erro: ".mysqli_connect_error());
   }
