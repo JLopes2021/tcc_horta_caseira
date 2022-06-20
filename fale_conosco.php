a7714a6af982187878bf2880c887faee33558034
Jefferson Lopes <81269229+JLopes2021@users.noreply.github.com>
2022-06-20 19:42:43 -0300

    upadete-20-06

--- fale_conosco.php
+++ fale_conosco.php
@@ -2,16 +2,17 @@
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Fale Conosco</title>
         <link rel="shortcut icon" type="imagex/png" href="fotos/Logo Tcc.png">
         <!-- Bootstrap CSS CDN -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
         <!-- Our Custom CSS -->
-        <link rel="stylesheet" href="css/pagina_inicial.css">
-        <link rel="stylesheet" href="css/faleconosco.css">
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+          <link rel="stylesheet" href="css/pagina_inicial.css">
+    <link rel="stylesheet" href="css/carrosel.css">
+    <link rel="stylesheet" href="css/rodape.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <!-- Font Awesome JS -->
         <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
@@ -21,7 +22,6 @@
          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
          <!-- Bootstrap JS -->
          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script> 
-         <script src="js/fale_conosco.js"></script>
     </head>
 <body>
     <div class="wrapper">
@@ -58,7 +58,13 @@
                         Sobre Nós
                     </a>
                     
-                </li>        
+                </li> 
+                                    <li>
+                        <a href="apresentacao-tcc.php">
+                            <i class="fas fa-book"></i>
+                            Nosso TCC
+                        </a>
+                    </li>        
             </ul>
         </nav>
 
@@ -90,7 +96,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="sobre_nos.php">Sobre Nós</a>
                             </li>
-                            
+                               <li class="nav-item">
+                               <a class="nav-link" href="apresentacao-tcc.php">Nosso TCC</a>
+                             </li>
                         </ul>
                     </div>
                 </div>
@@ -124,26 +132,77 @@
                         <br>
 
                       </div>
-
                       <button type="submit" class="btn btn-primary" name="botao" value="botao"  >Envie a sua mensagem</button><br>
                      
                     </form>
                     </div>
-
             </center>
-    
-    
-
     </div>
-    
+        </div>
+
+             </div>
+
+
+
+      
+   <footer id="myFooter">
+        <div class="container-fluid">
+            <div class="row">
+            <div class="col">
+                    <h5></h5>
+                    <ul>
+                       <li><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgZ3XxklIUzw0TzLuzYGfCTc-638poVOjTIilJUi0yrGPTGynF8KpaIRab1PdTCH3aWH1WxVH7kjp6c494_2ZOYX43O8gXkfrPUGYqgnIiuMJ7t35uej2GaVT8nMZ5wJYgkAR4OtPW0B5gi7vZmj6qtM5WVr66tljTizBAJWOdlcMiS3LfekicYWKNQjA/s320/Logo%20Tcc.png" height="150px" width="150px"></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h5>Inicio</h5>
+                    <ul>
+                        <li><a href="https://programadorviking.com.br/">Home</a></li>
+                        <li><a href="https://ceagesp.gov.br/">CEAGESP</a></li>
+                        <li><a href="https://www.sp.senac.br/cursos-livres/curso-de-horta-organica-caseira#:~:text=Voc%C3%AA%20aprender%C3%A1%20a%20realizar%20cultivo,o%20plantio%20e%20a%20manuten%C3%A7%C3%A3o.&text=Posso%20fazer%20este%20curso%3F,ser%20maior%20de%2018%20anos.">Cursos</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                <h5>Mapa do Site</h5>
+                    <ul>
+                        <li><a href="pagina_inicial.php">Página Inicial&nbsp;<i class="fa fa-home"></i></a></li>   
+                        <li><a href="comecar_horta.php">Começar Horta&nbsp;<i class="fa fa-seedling"></i></a></li>
+                        <li><a href="fale_conosco.php">Fale Conosco&nbsp;<i class="fas fa-paper-plane"></i></a></li> 
+                        <li><a href="sobre_nos.php">Sobre Nós&nbsp;<i class="fa fa-briefcase"></i></a></li>
+                    </ul>   
+                </div>
+                <div class="col">
+                    <h5>Suporte</h5>
+                    <ul>
+                        <li><a href="mailto: jeffinho.je8@gmail.com">FAQ</a></li>
+                    </ul>
+                </div>
+                
+                <div class="col">
+                <h5></h5>
+                    <ul>
+                       <li><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgZ3XxklIUzw0TzLuzYGfCTc-638poVOjTIilJUi0yrGPTGynF8KpaIRab1PdTCH3aWH1WxVH7kjp6c494_2ZOYX43O8gXkfrPUGYqgnIiuMJ7t35uej2GaVT8nMZ5wJYgkAR4OtPW0B5gi7vZmj6qtM5WVr66tljTizBAJWOdlcMiS3LfekicYWKNQjA/s320/Logo%20Tcc.png" height="150px" width="150px"></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+
+        
+    </footer>
+
+
+
    
     <script type="text/javascript">
         $(document).ready(function () {
     $('#botao-menu').on('click', function () {
         $('#sidebar').toggleClass('active');
-    });
-});
+                                            });
+                                        });
     </script>
+
+    
 </body>
 </html>
 
